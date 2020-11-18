45eec36fcc5a222fffe27c6a9dbb56aca36f7a5a
Mahery <74647071+maheryAlucard@users.noreply.github.com>
2020-11-18 11:46:21 +0300

    newUpdate

--- src/components/Myheader.js
+++ src/components/Myheader.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import { View, Text } from "react-native";
 
 import "../assets/css/header.css";
-
-class MyHeader extends Component {
-  render() {
-    return (
-      <div className="hmaincontainer">
-        <View>
-          <Text>MyHeader</Text>
-        </View>
-      </div>
-    );
-  }
-}
+var textStyle = {
+  color: "#fff"
+};
+const MyHeader = () => {
+  return (
+    <div className="hmaincontainer">
+      <View>
+        <Text style={textStyle}>My First Header</Text>
+      </View>
+    </div>
+  );
+};
 
 export default MyHeader;
